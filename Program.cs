6310139e5ef8dcbc95e126b4a7819e623ab81bcf
Julio Lozano <Julyodavyd@gmail.com>
2025-04-12 07:48:36 -0500

    Verción 1.0

---
+++ Program.cs
@@ -0,0 +1,141 @@
+﻿bool empezar = true;
+
+while (empezar)
+{
+    Console.WriteLine("***************  TIC-TAC-TOE GAME ***************");
+
+    // Crear Tabla
+    char[,] tablero = new char[,]
+    {
+    {'1','2','3'},
+    {'4','5','6'},
+    {'7','8','9'}
+    };
+   
+
+    imprimirTablero(tablero);
+
+    string JugadorN = "Jugador1";
+    char jugadorActual;
+
+    
+
+    while (true)
+    {
+        jugadorActual = JugadorN.Equals("Jugador1") ? 'X' : 'O';
+
+        Console.WriteLine($"> {JugadorN}, elige una posición (1-9)");
+        string? valor = Console.ReadLine();
+
+        if (int.TryParse(valor, out int numero))
+        {
+                    if (numero <= 0 || numero >= 10)
+                    {
+                        Console.WriteLine("----- Error : Ingrese un valor valido entre 1-9-----");
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                     ConsegirPosicion(numero,out int fila   ,out int columna);
+                        if (tablero[fila,columna]!='X' && tablero[fila,columna]!='O')
+                        {
+                        tablero[fila, columna] = jugadorActual;
+
+                    if (VerificarGanador(tablero))
+                    {
+                        Console.WriteLine($"******** {JugadorN}  gana!********* ");
+                        Console.WriteLine("Presione 1 para volver a jugar");
+                        var continuar = Console.ReadLine();
+                        empezar = continuar.Equals("1") ? true : false;
+                        
+                        break;
+                    }
+                    else if (VerificarEmpate(tablero))
+                    {
+                        Console.WriteLine("************* Empate! *************");
+                        Console.WriteLine("Presione 1 para volver a jugar");
+                        var continuar = Console.ReadLine();
+                        empezar = continuar.Equals("1") ? true : false;
+                        break;
+                    }
+
+                    imprimirTablero(tablero);
+
+                    
+
+
+                        JugadorN = JugadorN.Equals("Jugador1") ? "Jugador2" : "Jugador1";
+
+                } else
+                        {
+                            Console.WriteLine("Casilla ocupada");
+                        }
+
+                    }
+        }
+        else
+        {
+            Console.WriteLine("----- Error :Ingrese un numero valido ------");
+            Console.WriteLine();
+        }
+
+
+    }
+
+
+    void ConsegirPosicion(int posicion,out int fila, out int columna)
+    {
+        fila = (posicion - 1) / 3;
+        columna = (posicion - 1) % 3;
+    }
+
+    void imprimirTablero(char[,] tablero)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("                 ");
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($" {tablero[i, j]}  ");
+                if (j < 2) Console.Write("|");
+            }
+            Console.WriteLine();
+            if (i < 2)
+            {
+                Console.Write("                 ");
+                Console.Write("-------------");
+                Console.WriteLine();
+            }
+        }
+        Console.WriteLine();
+        Console.WriteLine("     Jugador1 : X       |       Jugador2 : O ");
+        Console.WriteLine();
+    }
+
+    bool VerificarEmpate(char[,] tablero)
+    {
+     foreach(char x in tablero)
+        {
+            if(x!='X' && x!='O')
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool VerificarGanador(char[,] tablero)
+    {
+        for (int i=0; i<3;i++)
+        {
+            if (tablero[i, 0] == tablero[i, 1] && tablero[i, 1] == tablero[i, 2]) return true;
+            if (tablero[0, i] == tablero[1, i] && tablero[1, i] == tablero[2, i]) return true;
+        }
+        if (tablero[0, 0] == tablero[1, 1] && tablero[1, 1] == tablero[2, 2]) return true;
+        if (tablero[0, 2] == tablero[1, 1] && tablero[1, 1] == tablero[2, 0]) return true;
+        return false;
+    }
+
+
+}
+
